b48c9f3c707fa4826e61451de71b276b3a127dab
Jackson Hong <jacksonhong0111@gmail.com>
2022-12-05 19:53:37 +0900

    initial commit

---
+++ src/main/java/com/jackson/clean/blog/CleanBlogApplication.java
@@ -0,0 +1,13 @@
+package com.jackson.clean.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanBlogApplication.class, args);
+    }
+
+}
